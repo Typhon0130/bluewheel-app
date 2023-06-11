a9d5033e7810a8b2b602ce1aef6562b0b7271793
Typhon <crystalmaster0130@gmail.com>
2023-06-11 20:03:13 +0900

    fix:binance api

--- src/pages/home/Hashrate.tsx
+++ src/pages/home/Hashrate.tsx
@@ -11,6 +11,9 @@ import {
   BodyItem,
   TableContent,
 } from "./style";
+// import axios from "axios";
+// import crypto from 'crypto';
+
 
 const hashHeadData = [
   "Model",
@@ -31,7 +34,55 @@ const bodyData = [
   " $6.60/day",
 ];
 
+// const apiKey = process.env.API_KEY;
+// const secretKey: string = process.env.SECRET_KEY || "";
+// const timestamp = Date.now();
+
+// interface MiningData {
+//   id: number;
+//   symbol: string;
+//   amount: number;
+//   createTime: number;
+// }
+
+
 const Hashrate = () => {
+
+  //   const queryString =
+  //     "userName=exampleUser&startDate=2022-01-01&endDate=2022-02-01";
+  //   const signature = crypto
+  //     .createHmac("sha256", secretKey)
+  //     .update(queryString + timestamp)
+  //     .digest("hex");
+
+  //   // construct the request config object
+  //   const requestConfig = {
+  //     url: `https://api.binance.com/sapi/v1/mining/statistics/user/list?${queryString}&timestamp=${timestamp}&signature=${signature}`,
+  //     headers: {
+  //       "X-MBX-APIKEY": apiKey,
+  //     },
+  //   };
+
+  // axios
+  //   .get(requestConfig.url, { headers: requestConfig.headers })
+  //   .then(res => {
+  //     console.log(`Request returned status code ${res.status}`);
+  //     console.log(`Response body:`, res.data);
+  //   })
+  //   .catch(err => {
+  //     console.error(`Error making request: ${err.message}`);
+  //   });
+
+  // const [miningData, setMiningData] = useState<MiningData[]>([]);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const response = await axios.get('https://api.binance.com/fapi/v1/mining/payment/list');
+  //     setMiningData(response.data);
+  //     console.log(response.data);
+  //   }
+  //   fetchData();
+  // }, []);
+
   return (
     <HashWrapper>
       <HashContainer>
